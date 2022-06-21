d9dbb52510baf1128d379b600678026a99b8554c
Kyelor Curtis <kyelorcurtis@Kyelors-MBP.attlocal.net>
2022-06-21 14:24:01 -0500

    Sample Project Code [Done]

---
+++ scholarbowlquestions/src/main/java/com/scholarbowlquestionproj/scholarbowlquestions/ScholarbowlquestionsApplication.java
@@ -0,0 +1,13 @@
+package com.scholarbowlquestionproj.scholarbowlquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarbowlquestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarbowlquestionsApplication.class, args);
+	}
+
+}
---
+++ scholarbowlquestions/src/test/java/com/scholarbowlquestionproj/scholarbowlquestions/ScholarbowlquestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholarbowlquestionproj.scholarbowlquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarbowlquestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
